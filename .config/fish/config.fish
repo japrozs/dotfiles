64089e58da4b83677894f74ca98f76fab6bcb2fe
Japroz-Saini <sainijaproz@gmail.com>
2021-10-22 09:40:13 +0530

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+fortune | cowsay -f tux
+set fish_color_command brwhite --bold
+set fish_color_host yellow
+set -g PATH $HOME/anaconda3/bin $PATH
+set -g PATH $HOME/anaconda3 $PATH
+set -g PATH /Users/pritpalsinghsaini/code/vcpkg/buildtrees/_vcpkg/build $PATH
+set -g PATH $HOME/depot_tools $PATH
+set -g PATH /usr/local/sbin $PATH
+set -g code $HOME/Documents/programming
+set -g proj $HOME/code/projects/
+set -g others $HOME/code/others/
+set -g git $HOME/code/github
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /usr/local/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+alias jsc="/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Helpers/jsc"
+set -g PATH /usr/local/opt/binutils/bin $PATH
+export NVM_DIR="$HOME/.nvm"
