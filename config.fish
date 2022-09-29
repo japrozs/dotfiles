7c466da9dd38e64af881768fd4170b9cda67cfac
Japroz Saini <sainijaproz@gmail.com>
2022-09-30 01:17:19 +0530

    Update config.fish

--- config.fish
+++ config.fish
@@ -2,7 +2,7 @@ fortune | cowsay -f tux
 set fish_color_command brwhite --bold
 set fish_color_host yellow
 set -gx HOMEBREW_NO_AUTO_UPDATE 1
-set -gx CLOUDINARY_URL 'cloudinary://315868988132497:uuY-fcLEdUz2OZv6ePGa45qeYJo@dl4o8eaha'
+set -gx CLOUDINARY_URL 'cloudinary://<INSERT_KEY_HERE>'
 
 function tree
 	find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
@@ -14,17 +14,22 @@ set -g rnd $HOME/rnd
 
 alias jsc="/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Helpers/jsc"
 alias netinfo="node -e  'console.table('(curl ipinfo.io | tr '\n' ' ' )')'"
+alias rmysql="mysql -uroot"
+alias dev="docker exec -it ecstatic_dubinsky bash"
 
 set -gx PATH $HOME/.cargo/bin/ $PATH
 set -gx LDFLAGS "$LDFLAGS -L/usr/local/opt/libedit/lib"
 # set -gx CPPFLAGS "$CPPFLAGS -I/usr/local/opt/libedit/include"
-set -gx PKG_CONFIG_PATH "/usr/local/opt/libedit/lib/pkgconfig"
+# set -gx PKG_CONFIG_PATH "/usr/local/opt/libedit/lib/pkgconfig"
 set -gx LDFLAGS "$LDFLAGS -L/usr/local/opt/llvm@12/lib"
 set -gx CPPFLAGS "$CPPFLAGS -I/usr/local/opt/llvm@12/include"
 set -gx LDFLAGS "$LDFLAGS -L/usr/local/opt/opencv@3/lib"
 # set -gx CPPFLAGS "$CPPFLAGS -I/usr/local/opt/opencv@3/include"
-set -gx PKG_CONFIG_PATH "$PKG_CONFIG_PATH /usr/local/opt/libedit/lib/pkgconfig"
-set -gx PKG_CONFIG_PATH "$PKG_CONFIG_PATH /usr/local/opt/opencv@3/lib/pkgconfig"
+# set -gx PKG_CONFIG_PATH "$PKG_CONFIG_PATH /usr/local/opt/libedit/lib/pkgconfig"
+set -gx PKG_CONFIG_PATH "/usr/local/opt/opencv@3/lib/pkgconfig"
+
+set -gx CMAKE_PREFIX_PATH "$CMAKE_PREFIX_PATH /usr/local/opt/qt@5"
+set -gx PATH "$PATH /usr/local/opt/qt@5/bin"
 
 export NVM_DIR="$HOME/.nvm"
 fish_add_path /usr/local/opt/llvm@12/bin
@@ -42,3 +47,5 @@ set -px --path PATH "/Users/pritpalsinghsaini/.bun/bin"
 function upload -d "Upload a file to https://keep.sh"
 	curl --upload-file $argv[1] https://free.keep.sh
 end
+
+set -gx OPENSSL_ROOT_DIR /usr/local/opt/openssl@3
