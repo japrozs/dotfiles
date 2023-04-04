7f1006ce981565140d2a95f6ff76cd15eae53f02
Japroz Saini <sainijaproz@gmail.com>
2023-04-04 14:20:31 +0530

    add `rnd-hash` function
    add `rnd-hash` function to generate a random hash

--- config.fish
+++ config.fish
@@ -8,14 +8,21 @@ function tree
 	find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
 end
 
+function rnd-hash -d 'generate a random 32bit md5sum hash'
+	string join '' $argv | md5sum | head -c 32
+end
+
 set -g code $HOME/coding/programming
 set -g git $code/github 
 set -g rnd $HOME/rnd
+set -g ctf $HOME/coding/ctf
+set -g notes $HOME/notes
+set -gx PYTHONPATH "/usr/local/lib/python3.9/site-packages"
 
 alias jsc="/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Helpers/jsc"
 alias netinfo="node -e  'console.table('(curl ipinfo.io | tr '\n' ' ' )')'"
 alias rmysql="mysql -uroot"
-alias dev="docker exec -it ecstatic_dubinsky bash"
+alias dev="multipass shell dev"
 
 set -gx PATH $HOME/.cargo/bin/ $PATH
 set -gx LDFLAGS "$LDFLAGS -L/usr/local/opt/libedit/lib"
@@ -27,9 +34,11 @@ set -gx LDFLAGS "$LDFLAGS -L/usr/local/opt/opencv@3/lib"
 # set -gx CPPFLAGS "$CPPFLAGS -I/usr/local/opt/opencv@3/include"
 # set -gx PKG_CONFIG_PATH "$PKG_CONFIG_PATH /usr/local/opt/libedit/lib/pkgconfig"
 set -gx PKG_CONFIG_PATH "/usr/local/opt/opencv@3/lib/pkgconfig"
+set -gx PKG_CONFIG_PATH "$PKG_CONFIG_PATH;/usr/local/Cellar/sdl2/2.0.20/lib/pkgconfig"
 
 set -gx CMAKE_PREFIX_PATH "$CMAKE_PREFIX_PATH /usr/local/opt/qt@5"
 set -gx PATH "$PATH /usr/local/opt/qt@5/bin"
+set -gx PATH "$PATH /usr/local/Cellar/dosfstools/4.2/sbin/"
 
 export NVM_DIR="$HOME/.nvm"
 fish_add_path /usr/local/opt/llvm@12/bin
